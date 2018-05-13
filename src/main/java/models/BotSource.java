5bad1d7a3c62b8de31bf73504562bab815623185
Konstantin <konstantin.turchenko@gmail.com>
2018-05-13 23:52:21 +0600

    Update BotSource.java

--- src/main/java/models/BotSource.java
+++ src/main/java/models/BotSource.java
@@ -1,8 +1,8 @@
 package models;
 
 public class BotSource {
-    private static final String BOTUSERNAME = "APC_Event_Bot";
-    private static final String BOTTOKEN = "555239457:AAEBlB75fcLci40PqDzgrkaCALmgjjCEpTU";
+    private static final String BOTUSERNAME = "________________";
+    private static final String BOTTOKEN = "_________________";
     private static final String BotTestAnswer = "тестовый ответ";
 
     public static String getBotTestAnswer() {
