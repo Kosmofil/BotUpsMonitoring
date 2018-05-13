be878aed15e2b21b4189c33e811e571285810aea
KONSTANTIN <konstantin.turchenko@gmail.com>
2018-05-13 22:32:56 +0600

    Created

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import controllers.ApcBot;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class Main  {
+
+    public static void main(String[] args) {
+        ApcBot.runBot();
+    }
+
+}
---
+++ src/main/java/models/BotSource.java
@@ -0,0 +1,14 @@
+package models;
+
+public class BotSource {
+    private final String BOTUSERNAME = "APC_Event_Bot";
+    private final String BOTTOKEN = "555239457:AAEBlB75fcLci40PqDzgrkaCALmgjjCEpTU";
+
+    public String getBOTUSERNAME() {
+        return BOTUSERNAME;
+    }
+
+    public String getBOTTOKEN() {
+        return BOTTOKEN;
+    }
+}
